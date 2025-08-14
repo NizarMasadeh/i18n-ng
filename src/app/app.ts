687634a9ee2a4c2fd1a48baa89e74844e9793d2b
Nizar Masadeh <nizarmasadeh2001@gmail.com>
2025-08-14 09:50:35 +0300

    en-US "default" + ar-JO "configuration"

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,13 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [
+    CommonModule
+  ],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected title = 'i18n';
 }
